1f7d781ccc6dac397109e6efaa73ee4c93109668
Alex Kras <akras14@gmail.com>
2016-09-28 09:33:18 -0700

    Add support for requireing sass

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,28 @@ require('webpack');
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, 'src/main.es6')
+    main: path.resolve(__dirname, 'src/main.es6'),
   },
   output: {
     path: path.resolve(__dirname, './build/'),
     filename: '[name].bundle.js'
   },
   module: {
-    loaders: [{
-      test: /\.es6?$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015']
+    loaders: [
+      {
+        test: /\.es6?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ["style-loader", "css-loader", "sass"]
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.es6']
